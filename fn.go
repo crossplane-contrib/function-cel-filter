59cfea70e1605b959de302d33a0f6e2ac9276e86
Nic Cope <nicc@rk0n.org>
2024-02-17 18:53:57 -0800

    Move to crossplane-contrib
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- fn.go
+++ fn.go
@@ -12,7 +12,8 @@ import (
 	fnv1beta1 "github.com/crossplane/function-sdk-go/proto/v1beta1"
 	"github.com/crossplane/function-sdk-go/request"
 	"github.com/crossplane/function-sdk-go/response"
-	"github.com/crossplane/function-template-go/input/v1beta1"
+
+	"github.com/crossplane-contrib/function-cel-filter/input/v1beta1"
 )
 
 // Function returns whatever response you ask it to.
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/crossplane/function-template-go
+module github.com/crossplane-contrib/function-cel-filter
 
 go 1.21
 
